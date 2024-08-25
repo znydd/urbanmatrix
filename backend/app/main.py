2c0f040e81e7625cca38a8183cc3ce19be2aa8b0
Znyd <mailboxofnabil@gmail.com>
2024-08-26 04:42:46 +0600

    [BE] fixed cors and setup router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from app.api.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
+from app.db.databse import get_db
 
 app = FastAPI()
 
-app.include_router()
 
-@app.get("/")
-def hello():
-    return {"hello": "brothers"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# @app.get("/user")
+# def read_items(db = Depends(get_db)):
+#     with db.cursor() as cursor:
+#         cursor.execute("SELECT * FROM users")
+#         items = cursor.fetchall()
+#     return items
+
+app.include_router(api_router, prefix="/api")
