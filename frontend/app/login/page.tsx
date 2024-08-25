4ea8ab9f95ec840630148d51123ed57173a92b7c
Znyd <mailboxofnabil@gmail.com>
2024-08-25 06:21:45 +0600

    [FE] login page without fn

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,32 @@
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+export default function Login() {
+  return (
+    <div className=" flex items-center justify-center min-h-screen bg-gray-100">
+    <Card className="mx-auto max-w-sm">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl font-bold">Login</CardTitle>
+        <CardDescription>Enter your email and password to login to your account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" required />
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+    </div>
+  )
+}
