eb4c4ef3fd54b9257aeac77ec7c71af23dcce99d
Znyd <mailboxofnabil@gmail.com>
2024-08-27 05:05:55 +0600

    [BE] /user router to test protected route fn

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends
-from app.api.router import api_router
 from fastapi.middleware.cors import CORSMiddleware
+from app.api.router import api_router
 from app.db.databse import get_db
+from app.utils.security import get_current_user
 
 app = FastAPI()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,11 +14,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# @app.get("/user")
-# def read_items(db = Depends(get_db)):
-#     with db.cursor() as cursor:
-#         cursor.execute("SELECT * FROM users")
-#         items = cursor.fetchall()
-#     return items
+#for testing
+@app.get("/user")
+def read_items(db = Depends(get_db), current_user: dict = Depends(get_current_user) ):
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM users")
+        items = cursor.fetchall()
+    print(current_user)
+    return items
 
 app.include_router(api_router, prefix="/api")
