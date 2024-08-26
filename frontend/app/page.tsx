a1d365864e303f67a6064c819e935214e11f787b
Znyd <mailboxofnabil@gmail.com>
2024-08-27 05:09:41 +0600

    [FE] formated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,13 +3,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-  <>
-  <Button asChild>
-    <Link href="/login">Login</Link>
-  </Button>
-  <Button asChild>
-    <Link href="/signup">SignUp</Link>
-  </Button>
-  </>
+    <>
+      <Button asChild>
+        <Link href="/login">Login</Link>
+      </Button>
+      <Button asChild>
+        <Link href="/signup">SignUp</Link>
+      </Button>
+    </>
   );
 }
