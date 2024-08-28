3c586f75ee740d5785b943b43c532b948012dbd7
Znyd <mailboxofnabil@gmail.com>
2024-08-28 10:09:36 +0600

    [FE] after logout it disable the browser back btn

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,27 @@
+"use client"
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    history.pushState(null, '', "/");
+    window.addEventListener('popstate', function (event) {
+        history.pushState(null, '', "/");
+    });
+}, []);  
   return (
     <>
+    <div className="flex flex-row min-h-screen justify-center items-center gap-x-4">
       <Button asChild>
         <Link href="/login">Login</Link>
       </Button>
       <Button asChild>
         <Link href="/signup">SignUp</Link>
       </Button>
+      </div>
     </>
   );
 }
