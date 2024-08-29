3736b4f8cb5691f794c765443e7661aec5b65fa1
Znyd <mailboxofnabil@gmail.com>
2024-08-30 04:01:13 +0600

    [BE] Included router for /user

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.endpoints import auth
+from app.api.endpoints import auth, user
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(user.router, prefix="/user", tags=["user"])
