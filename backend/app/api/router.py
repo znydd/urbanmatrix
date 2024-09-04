d3004a2b05673c2b1b6221e3210442e7c43a175c
Znyd <mailboxofnabil@gmail.com>
2024-09-05 04:01:02 +0600

    [BE] fix registration send role = user as response

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
-from app.api.endpoints import auth, user, admin
+from app.api.endpoints import auth, user, admin, search_docs
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
+api_router.include_router(search_docs.router, prefix="/searchdocs", tags=["searchdocs"])
 
 
 # Admin route
