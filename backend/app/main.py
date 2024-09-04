e409db62210ff56270053658c0db46c371a89492
Znyd <mailboxofnabil@gmail.com>
2024-09-05 03:59:50 +0600

    [BE] sending image(docs) from database

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,8 @@ from app.api.router import api_router
 from app.db.databse import get_db
 from app.utils.security import get_current_user
 
+from fastapi.responses import FileResponse
+
 app = FastAPI()
 
 app.add_middleware(
@@ -17,14 +19,21 @@ app.add_middleware(
 #for testing
 @app.get("/")
 def read_items():
-
     return {"hello":"world"}
+
+@app.get("/download")
+async def download_image():
+    file_path = "docsbucket/birth_certificate/100005_birth_certificate.png"
+    return FileResponse(file_path, media_type="application/octet-stream", filename="nabil_bc.png")
+    pass
+
+
 @app.get("/user")
 def read_items(db = Depends(get_db), current_user: dict = Depends(get_current_user)):
     with db.cursor() as cursor:
         cursor.execute("SELECT * FROM users")
         items = cursor.fetchall()
-    print(current_user)
+    # print(current_user)
     return items
 
 app.include_router(api_router, prefix="/api")
