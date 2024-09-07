e799e5227bc67b2ba84628dff20e8d181ff99d72
Znyd <mailboxofnabil@gmail.com>
2024-09-08 00:48:56 +0600

    [FE] idk just white space

--- frontend/app/(auth)/getdocs/page.tsx
+++ frontend/app/(auth)/getdocs/page.tsx
@@ -18,7 +18,7 @@ interface Doc {
     type: string;
     no: number;
     file_name: string;
-  }
+}
 
 
 export default function GetDocs() {
@@ -41,76 +41,76 @@ export default function GetDocs() {
             const docsResp = await savedDocs()
             setDocs(docsResp)
         } catch (error) {
-            
+
         }
     }
 
     const handleDownload = async () => {
         try {
-          const url = await downloadNid()
-          if (typeof url !== 'string') {
-            console.error('Download URL is undefined or not a string');
-            return;
-          }
-          const link = document.createElement('a');
-          link.href = url;
-          link.setAttribute('download', "nid.png");
-          document.body.appendChild(link);
-          link.click();
-          
-          // Clean up
-          window.URL.revokeObjectURL(url);
-          link.remove();
-          
-        } catch (error) {
-          console.error('Error downloading file:', error);
-        }
-      }
-
-      const handleDeathBirthDownload = async (type: string, file: string, name: string) => {
-        if(type === "Birth Certificate"){
-        try {
-          const url = await downloadBirthCert(file)
-    
-          if (typeof url !== 'string') {
-            console.error('Download URL is undefined or not a string');
-            return;
-          }
-          const link = document.createElement('a');
-          link.href = url;
-          link.setAttribute('download', `${name}_${file}`);
-          document.body.appendChild(link);
-          link.click();
-          
-          // Clean up
-          window.URL.revokeObjectURL(url);
-          link.remove();
-          
-        } catch (error) {
-          console.error('Error downloading file:', error);
-        }
-      }else{
-        try {
-            const url = await downloadDeathCert(file)
-      
+            const url = await downloadNid()
             if (typeof url !== 'string') {
-              console.error('Download URL is undefined or not a string');
-              return;
+                console.error('Download URL is undefined or not a string');
+                return;
             }
             const link = document.createElement('a');
             link.href = url;
-            link.setAttribute('download', `${name}_${file}`);
+            link.setAttribute('download', "nid.png");
             document.body.appendChild(link);
             link.click();
-            
+
             // Clean up
             window.URL.revokeObjectURL(url);
             link.remove();
-            
-          } catch (error) {
+
+        } catch (error) {
             console.error('Error downloading file:', error);
-          }
-      }
+        }
+    }
+
+    const handleDeathBirthDownload = async (type: string, file: string, name: string) => {
+        if (type === "Birth Certificate") {
+            try {
+                const url = await downloadBirthCert(file)
+
+                if (typeof url !== 'string') {
+                    console.error('Download URL is undefined or not a string');
+                    return;
+                }
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', `${name}_${file}`);
+                document.body.appendChild(link);
+                link.click();
+
+                // Clean up
+                window.URL.revokeObjectURL(url);
+                link.remove();
+
+            } catch (error) {
+                console.error('Error downloading file:', error);
+            }
+        } else {
+            try {
+                const url = await downloadDeathCert(file)
+
+                if (typeof url !== 'string') {
+                    console.error('Download URL is undefined or not a string');
+                    return;
+                }
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', `${name}_${file}`);
+                document.body.appendChild(link);
+                link.click();
+
+                // Clean up
+                window.URL.revokeObjectURL(url);
+                link.remove();
+
+            } catch (error) {
+                console.error('Error downloading file:', error);
+            }
+        }
     }
     console.log(userInfo)
     const logout = () => {
@@ -130,7 +130,7 @@ export default function GetDocs() {
                             <Button variant="outline">{userInfo ? `${userInfo.email}` : 'Loading...'}</Button>
                         </div>
                     </CardHeader>
-                     <Card className='min-h-[650px] w-full flex flex-row p-4 gap-2'>
+                    <Card className='min-h-[650px] w-full flex flex-row p-4 gap-2'>
                         <Card className=' bg-slate-50 w-1/2 p-10'>
                             <CardContent>
                                 <div className="grid grid-cols-3 gap-4">
@@ -150,27 +150,27 @@ export default function GetDocs() {
                                 <CardTitle className="text-lg">Saved Docs</CardTitle>
                             </CardHeader>
                             <ScrollArea className=' flex-grow'>
-                            <CardContent className="space-y-2">
-                                {docs.map((doc, index) => (
-                                    <div
-                                        key={index}
-                                        className={`flex justify-between items-center p-2 rounded-md ${doc.type === "Death Certificate" ? "bg-yellow-100" : "bg-green-100"
-                                            }`}
-                                    >
-                                        <div>
-                                            <p className="font-medium">{doc.type}</p>
-                                            <p className="text-sm text-gray-600">{doc.name}</p>
+                                <CardContent className="space-y-2">
+                                    {docs.map((doc, index) => (
+                                        <div
+                                            key={index}
+                                            className={`flex justify-between items-center p-2 rounded-md ${doc.type === "Death Certificate" ? "bg-yellow-100" : "bg-green-100"
+                                                }`}
+                                        >
+                                            <div>
+                                                <p className="font-medium">{doc.type}</p>
+                                                <p className="text-sm text-gray-600">{doc.name}</p>
+                                            </div>
+                                            <Button onClick={() => handleDeathBirthDownload(doc.type, doc.file_name, doc.name)} variant="outline" size="sm">
+                                                Download
+                                            </Button>
                                         </div>
-                                        <Button onClick={() => handleDeathBirthDownload(doc.type, doc.file_name, doc.name)} variant="outline" size="sm">
-                                            Download
-                                        </Button>
-                                    </div>
-                                ))}
-                            </CardContent>
+                                    ))}
+                                </CardContent>
                             </ScrollArea>
                         </Card>
                     </Card>
-                    
+
                     <Button className=' bg-red-500 mt-4' onClick={logout}>
                         Logout
                     </Button>
