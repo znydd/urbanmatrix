0160457f6eb3685b1eec0890e2973f24418b0d96
Znyd <mailboxofnabil@gmail.com>
2024-09-09 03:25:35 +0600

    [FE] frontend api call birth apply implemented

---
+++ frontend/utils/registerdocsApi.ts
@@ -0,0 +1,37 @@
+import { AxiosError } from "axios";
+import axios from "axios";
+
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL; 
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+      'Content-Type': 'maltipart/form-data',
+    },
+  });
+
+const setAuthToken = (token: string | null) => {
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common['Authorization'];
+  }
+};
+
+
+export const registerBithCert = async (formData: FormData) => {
+    const token = localStorage.getItem('token')
+    setAuthToken(token)
+   
+    try {
+      const response = await api.post('api/registerdocs/birthcert', formData);
+      console.log(response.statusText)
+      return response.statusText
+    } catch (error) {
+      if (error instanceof AxiosError && error.response) {
+        const errorMessage: string = error.response.data.detail || 'Api failed for create birth certificate';
+        return errorMessage
+    }
+  }
+  };
